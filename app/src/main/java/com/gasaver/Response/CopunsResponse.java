7f916e3a4d4886f1e21cf4766467041a413b992d
abhishek493 <abhishek@aakrutisolutions.com>
2023-04-10 18:00:37 +0530

    Gasaver AS2

--- app/src/main/java/com/gasaver/Response/CopunsResponse.java
+++ app/src/main/java/com/gasaver/Response/CopunsResponse.java
@@ -7,6 +7,9 @@ import java.util.ArrayList;
 
 public class CopunsResponse {
 
+    public static String BASE_URL = "";
+
+
     @SerializedName("copuns_details")
     @Expose
 //    private List<CopunsDetail> copunsDetails;
